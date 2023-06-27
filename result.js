6ac8139f273d6ea1b7c33d3ce4ba36eb1d592689
MikeSusutZZ <mike.susut@gmail.com>
2023-06-27 01:06:32 -0700

    Pushing a bunch of stuff

--- result.js
+++ result.js
@@ -1,9 +1,10 @@
 module.exports = function (app, db) {
     app.get("/result", async (req, res) => {
-        const code = req.query.code;
-        const num = req.query.num;
-        const oppNum = num == player1 ? player2 : player1;
-        const name = req.query.name;
+        const code = req.session.code;
+        const num = req.session.num;
+        const name = req.session.name;
+        const oppNum = num == "player1" ? "player2" : "player1";
+
         const room = await db.findOne({ code: code });
         // validate query hasn't been tampered with
         if (room[num].name != name) {
@@ -11,19 +12,19 @@ module.exports = function (app, db) {
             return;
         }
         var you = room[num];
-        
-        opp = room[oppNum];
-        
+
+        var opp = room[oppNum];
+
         var yourPick = num + "action";
         var oppPick = oppNum + "action";
         yourPick = room[yourPick];
         oppPick = room[oppPick];
 
         var message = "";
-        await update(num, "shieldCool", false);
+        await update(num, "shieldCool", false, code);
 
         if (yourPick == "Shield") {
-            await update(num, "shieldCool", true);
+            await update(num, "shieldCool", true, code);
 
             if (oppPick == "Shoot" || oppPick == "Stab" || oppPick == "Rush") {
                 message = "You defended thier attack";
@@ -31,7 +32,7 @@ module.exports = function (app, db) {
         }
 
         if (yourPick == "Block") {
-            await update(num, "blockSpent", true);
+            await update(num, "blockSpent", true, code);
 
             if (oppPick == "Shoot" || oppPick == "Stab" || oppPick == "Rush") {
                 message = "You defended thier attack";
@@ -39,23 +40,23 @@ module.exports = function (app, db) {
         }
 
         if (yourPick == "Parry") {
-            update(num, "parrySpent", true);
+            await update(num, "parrySpent", true, code);
             if (oppPick == "Stab") {
                 if (opp.stabbed) {
                     message = "You parried their attack, but didn't land a strike"
                 } else {
-                    await update(oppNum, "stabbed", true);
+                    await update(oppNum, "stabbed", true, code);
                     message = "You parried their attack and struck back";
                 }
             }
         }
 
         if (yourPick == "Shoot") {
-            await update(num, "charged", false);
+            await update(num, "charged", false, code);
             if (oppPick == "Shield" || oppPick == "Block") {
                 message = "Your attack was defended";
             } else {
-                await hurt(opp);
+                await hurt(opp, code);
             }
             if (oppPick == "Stab" || oppPick == "Rush") {
                 message = "You shot your opponent as they were coming to attack you";
@@ -68,13 +69,13 @@ module.exports = function (app, db) {
         }
 
         if (yourPick == "Rush") {
-            await update(num, "blockSpent", true);
+            await update(num, "blockSpent", true, code);
             if (oppPick == "Shield" || oppPick == "Block") {
                 message = "Your attack was defended";
             } else if (oppPick == "Shoot") {
                 message = "You were shot before you could reach your opponent";
             } else {
-                await hurt(opp)
+                await hurt(opp, code)
                 message = "You rushed down and killed your opponent"
             }
 
@@ -98,13 +99,13 @@ module.exports = function (app, db) {
         }
 
         if (yourPick == "Charge") {
-            await update(num, "charged", true);
-            if(oppPick == "Shoot"){
+            await update(num, "charged", true, code);
+            if (oppPick == "Shoot") {
                 message = "You were shot ";
             }
-            else if(oppPick == "Stab" && you.stabbed || oppPick == "Rush"){
+            else if (oppPick == "Stab" && you.stabbed || oppPick == "Rush") {
                 message = "You were mortally wounded "
-            } else if(oppPick == "Stab"){
+            } else if (oppPick == "Stab") {
                 message = "You were wounded "
             }
             else {
@@ -113,15 +114,17 @@ module.exports = function (app, db) {
             message += "while you were charging"
         }
 
+        res.render("result", { message: message })
+
     })
 
-    async function update(playerNum, key, value) {
+    async function update(playerNum, key, value, code) {
         var field = `${playerNum}.${key}`
         await db.updateOne({ code: code }, { $set: { [field]: value } });
     }
 
 
-    async function hurt(player) {
+    async function hurt(player, code) {
         player.health -= 1;
         player.charged = false;
         player.stabbed = false;
